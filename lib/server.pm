19624c66afb92f872ce066ad538b4ce960008289
Travis Chase <travis.chase@gmail.com>
2018-06-03 13:39:30 -0600

    removed _call_ev3 removed left / right motor setting instead just detecting motors attached updated unit test

--- lib/server.pm
+++ lib/server.pm
@@ -202,80 +202,21 @@ sub __hello {
     };
 }
 
-sub __set_left_motor__meta {
-    return {
-        description => 'Set the left motor for movement control',
-        port        => {
-            description => 'The port number the left motor is connected to',
-            test        => sub {
-                my $data = shift;
-
-                die "Not a valid port" unless defined $data && $data =~ /^\d$/;
-            },
-        },
-    };
-}
-
-sub __set_left_motor {
-    my $self    = shift;
-    my $request = shift;
-
-    my $fh;
-    open $fh, '>', $self->_left_motor_file || die 'Could not open left motor file for writing';
-    print $fh $request->{'port'};
-    close $fh;
-
-    return {
-        l_motor => $request->{'port'},
-    };
-}
-
-sub __set_right_motor__meta {
-    return {
-        description => 'Set the right motor for movement control',
-        port        => {
-            description => 'The port number the right motor is connected to',
-            test        => sub {
-                my $data = shift;
-
-                die "Not a valid port" unless defined $data && $data =~ /^\d$/;
-            },
-        },
-    };
-}
-
-sub __set_right_motor {
-    my $self    = shift;
-    my $request = shift;
-
-    my $fh;
-    open $fh, '>', $self->_right_motor_file || die 'Could not open right motor file for writing';
-    print $fh $request->{'port'};
-    close $fh;
-
-    return {
-        r_motor => $request->{'port'},
-    };
-}
-
 sub __position__meta {
     my $self = shift;
 
-    my ($r_motor, $l_motor) = @{ $self->_get_large_motors };
+    my ($r_motor, $l_motor) = @{ $self->_large_motors };
     $r_motor = defined $r_motor ? $r_motor : -1;
     $l_motor = defined $l_motor ? $l_motor : -1;
 
     return {
         description => 'Get motor position',
-        port        => {
-            description => 'The port number the motor is connected to',
+        sequence_number  => {
+            description => 'The sequence number the motor is identified as',,
             test        => sub {
                 my $data = shift;
 
-                die "Not a valid port" unless
-                    defined $data &&
-                    $data =~ /^\d$/ &&
-                    ( $data == $r_motor || $data == $l_motor );
+                die "Not a valid sequence_number" unless defined $data && $data =~ /^\d+$/;
             },
         },
     };
@@ -286,23 +227,13 @@ sub __position {
     my $request = shift;
 
     return {
-        position => $self->_call_ev3(get_tacho_position => [ $request->{'port'} ])->[0],
+        position => ev3::get_tacho_position($request->{'sequence_number'}),
     };
 }
 
 sub __move__meta {
     return {
         description => 'Move the robot forward some distance at some speed',
-        degrees => {
-            description => 'How far, in degrees of rotation of the motor, to move the robot. Pass -1 to run until a stop movement request is made.',
-            test        => sub {
-                my $data = shift;
-
-                die "Not valid amount of degrees" unless defined $data && $data =~ /^[-]?\d+$/;
-
-                die "Not valid amount of degrees" unless $data == -1 || $data >= 0;
-            },
-        },
         speed => {
             description => 'How fast, between -100 and 100, to move the robot.',
             test        => sub {
@@ -319,31 +250,19 @@ sub __move {
     my $request = shift;
 
     my $position = {};
-    for my $sn ( @{ $self->_get_large_motors } ) {
+    for my $sn ( @{ $self->_large_motors } ) {
         # set the speed
-        $self->_call_ev3(set_tacho_duty_cycle_sp => [ $sn, $request->{'speed'} ]);
+        ev3::set_tacho_duty_cycle_sp( $sn, $request->{'speed'} );
 
         # disable ramp up and ramp down
-        $self->_call_ev3(set_tacho_ramp_up_sp => [ $sn, 0 ]);
-        $self->_call_ev3(set_tacho_ramp_down_sp => [ $sn, 0 ]);
+        ev3::set_tacho_ramp_up_sp( $sn, 0 );
+        ev3::set_tacho_ramp_down_sp( $sn, 0 );
 
         # track the starting position of each motor
-        $position->{$sn} = $self->_call_ev3(get_tacho_position => [ $sn ])->[0];
+        $position->{$sn} = ev3::get_tacho_position( $sn );
 
-        if ($request->{'degrees'} == -1) {
-            # run command
-            $self->_call_ev3(set_tacho_command_inx => [ $sn, $ev3::TACHO_RUN_FOREVER ]);
-
-            $position->{$sn} = 'nil'; # since we move until a stop request we don't know the distance
-        } else {
-            my $tacho_count = $self->_degrees_to_tacho_count($request->{'degrees'});
-
-            # set target relative position
-            $self->_call_ev3(set_tacho_position_sp => [ $sn, $tacho_count ]);
-
-            # run command
-            $self->_call_ev3(set_tacho_command_inx => [ $sn, $ev3::TACHO_RUN_TO_REL_POS ]);
-        }
+        # run command
+        ev3::set_tacho_command_inx( $sn, $ev3::TACHO_RUN_FOREVER );
     }
 
     return {
@@ -354,14 +273,6 @@ sub __move {
 sub __turn__meta {
     return {
         description => 'Turn the robot some amount based on motor degrees and at some speed',
-        degrees => {
-            description => 'How far, in degrees of rotation of the motor, to turn the robot.',
-            test        => sub {
-                my $data = shift;
-
-                die "Not valid amount of degrees" unless defined $data && $data =~ /^\d+$/;
-            },
-        },
         speed => {
             description => 'How fast, between -100 and 100, to move the robot.',
             test        => sub {
@@ -379,24 +290,19 @@ sub __turn {
 
     my $position = {};
     my $speed = $request->{'speed'}; # alternate speed between positive and negative values for each motor
-    for my $sn ( @{ $self->_get_large_motors } ) {
+    for my $sn ( @{ $self->_large_motors } ) {
         # set the speed
-        $self->_call_ev3(set_tacho_duty_cycle_sp => [ $sn, $speed ]);
+        ev3::set_tacho_duty_cycle_sp( $sn, $speed );
 
         # disable ramp up and ramp down
-        $self->_call_ev3(set_tacho_ramp_up_sp => [ $sn, 0 ]);
-        $self->_call_ev3(set_tacho_ramp_down_sp => [ $sn, 0 ]);
+        ev3::set_tacho_ramp_up_sp( $sn, 0 );
+        ev3::set_tacho_ramp_down_sp( $sn, 0 );
 
         # track the starting position of each motor
-        $position->{$sn} = $self->_call_ev3(get_tacho_position => [ $sn ])->[0];
-
-        my $tacho_count = $self->_degrees_to_tacho_count($request->{'degrees'});
-
-        # set target relative position
-        $self->_call_ev3(set_tacho_position_sp => [ $sn, $tacho_count ]);
+        $position->{$sn} = ev3::get_tacho_position( $sn );
 
         # run command
-        $self->_call_ev3(set_tacho_command_inx => [ $sn, $ev3::TACHO_RUN_TO_REL_POS ]);
+        ev3::set_tacho_command_inx( $sn, $ev3::TACHO_RUN_FOREVER );
 
         # since we are turning speed must be negated so that the next motors speed is opposite this motor's speed
         $speed = -$speed;
@@ -417,9 +323,9 @@ sub __stop {
     my $self    = shift;
     my $request = shift;
 
-    for my $sn ( @{ $self->_get_large_motors } ) {
+    for my $sn ( @{ $self->_large_motors } ) {
         # run command
-        $self->_call_ev3(set_tacho_command_inx => [ $sn, $ev3::TACHO_STOP ]);
+        ev3::set_tacho_command_inx( $sn, $ev3::TACHO_STOP );
     }
 
     return {};
@@ -555,58 +461,18 @@ sub _ev3_uninit {
     }
 }
 
-sub _call_ev3 {
-    my $self   = shift;
-    my $method = shift;
-    my $args   = shift || [];
-
-    die 'args are not an arrayref' unless $args && ref $args eq 'ARRAY';
-
-    die 'invalid method' unless $method;
-
-    NO_STRICT_REFS: {
-        no strict 'refs';
-
-        die 'invalid method' unless ${'ev3::'}{$method};
-
-        my @result = ( eval { ${'ev3::'}{$method}( @$args ) } );
-
-        return \@result || $@;
-    }
-}
-
-sub _left_motor_file { './LEFTMOTOR.txt' }
-
-sub _right_motor_file { './RIGHTMOTOR.txt' }
-
-sub _has_large_motors_set {
+sub _large_motors {
     my $self = shift;
-
-    return defined $self->_get_large_motors->[0] && defined $self->_get_large_motors->[1];
-}
-
-sub _get_large_motors {
-    my $self = shift;
-
-    return [] unless -f $self->_left_motor_file && -f $self->_right_motor_file;
 
     return $self->{'_large_motors'} //= do {
-        my ($l_motor, $r_motor);
+        my @motors;
+        for ( my $i = 0; $i < $ev3::TACHO_DESC__LIMIT_; $i++ ) {
+            my $type_inx = ev3::ev3_tacho_desc_type_inx( $i );
 
-        my $fh;
-        open $fh, '<', $self->_left_motor_file || die 'Could not open left motor file for reading';
-        $l_motor = <$fh>;
-        close $fh;
+            push @motors, $i if $type_inx != $ev3::TACHO_TYPE__NONE_;
+        }
 
-        die 'Left motor not defined' unless defined $l_motor;
-
-        open $fh, '<', $self->_right_motor_file || die 'Could not open right motor file for reading';
-        $r_motor = <$fh>;
-        close $fh;
-
-        die 'Right motor not defined' unless defined $r_motor;
-
-        [ $l_motor, $r_motor ];
+        \@motors;
     };
 }
 
@@ -616,7 +482,7 @@ sub _degrees_to_tacho_count {
 
     my $ONE_ROTATION = 360; # degrees
 
-    my $tacho_count = $self->_call_ev3(get_tacho_count_per_rot => [])->[0];
+    my $tacho_count = ev3::get_tacho_count_per_rot();
 
     return $degrees * $tacho_count / $ONE_ROTATION;
 }
--- t/server.t
+++ t/server.t
@@ -17,7 +17,7 @@ use JSON::XS;
 
 use ev3mock;
 
-use Test::More tests => 35;
+use Test::More tests => 19;
 
 my $ev3mock = ev3mock->new();
 
@@ -35,26 +35,8 @@ my $resp = decode_json($server->process_http_request({
 
 ok($resp->{'success'} == 0, 'invalid method returns error');
 
-CALL_EV3: {
-    eval { $server->_call_ev3(this_should_fail => []) };
-
-    ok($@, '_call_ev3 dies on invalid methods');
-
-    my $resp = $server->_call_ev3(ev3_init => []);
-
-    ok($resp && $server->{'_ev3_initialized'}, '_call_ev3 initializes ev3');
-}
-
 DEGREES_TO_TACHO_COUNT: {
     ok($server->_degrees_to_tacho_count(90), '_degrees_to_tacho_count returns a value');
-}
-
-CLEAR_MOTOR_FILES: {
-    unlink $server->_left_motor_file;
-    unlink $server->_right_motor_file;
-
-    ok(!-f $server->_left_motor_file, "left motor file does not exist");
-    ok(!-f $server->_right_motor_file, "right motor file does not exist");
 }
 
 HELLO: {
@@ -65,10 +47,6 @@ HELLO: {
     ok($resp->{'hello'}, 'hello method returned successfully');
 }
 
-HAS_LARGE_MOTORS_SET_FALSE: {
-    ok(! $server->_has_large_motors_set, '_has_large_motors_set is false');
-}
-
 AVAILABLE_MOTORS: {
     $resp = decode_json($server->process_http_request({
         method => "available_motors",
@@ -77,44 +55,8 @@ AVAILABLE_MOTORS: {
     ok($resp->{'success'}, 'available_motors method returned successfully');
 }
 
-SET_LEFT_MOTOR: {
-    $resp = decode_json($server->process_http_request({
-        method => "set_left_motor",
-    }));
-
-    ok(!$resp->{'success'}, 'set_left_motor fails without port');
-
-    $resp = decode_json($server->process_http_request({
-        method => "set_left_motor",
-        port   => 0,
-    }));
-
-    ok($resp->{'success'}, 'set_left_motor method returned successfully');
-}
-
-SET_RIGHT_MOTOR: {
-    $resp = decode_json($server->process_http_request({
-        method => "set_right_motor",
-    }));
-
-    ok(!$resp->{'success'}, 'set_right_motor fails without port');
-
-    $resp = decode_json($server->process_http_request({
-        method => "set_right_motor",
-        port   => 0,
-    }));
-
-    ok($resp->{'success'}, 'set_right_motor method returned successfully');
-}
-
-HAS_LARGE_MOTORS_SET_TRUE: {
-    ok($server->_has_large_motors_set, '_has_large_motors_set is true');
-}
-
-GET_LARGE_MOTORS: {
-    ok(ref $server->_get_large_motors eq 'ARRAY', '_get_large_motors returns an array ref');
-    ok(defined $server->_get_large_motors->[0], '_get_large_motors l_motor value is defined');
-    ok(defined $server->_get_large_motors->[1], '_get_large_motors r_motor value is defined');
+LARGE_MOTORS: {
+    ok(ref $server->_large_motors eq 'ARRAY', '_large_motors returns an array ref');
 }
 
 POSITION: {
@@ -122,11 +64,11 @@ POSITION: {
         method => "position"
     }));
 
-    ok(!$resp->{'success'}, 'position fails without port');
+    ok(!$resp->{'success'}, 'position fails without sequence number');
 
     $resp = decode_json($server->process_http_request({
-        method => "position",
-        port   => 0,
+        method          => "position",
+        sequence_number => 0,
     }));
 
     ok($resp->{'success'}, 'position method returns successfully');
@@ -138,18 +80,8 @@ MOVE: {
         method => "move"
     }));
 
-    ok(!$resp->{'success'}, 'move fails without degrees');
-
     $resp = decode_json($server->process_http_request({
         method  => "move",
-        degrees => -2,
-    }));
-
-    ok(!$resp->{'success'}, 'move fails with invalid degrees');
-
-    $resp = decode_json($server->process_http_request({
-        method  => "move",
-        degrees => -1,
         speed   => 200,
     }));
 
@@ -162,7 +94,6 @@ MOVE: {
 
     $resp = decode_json($server->process_http_request({
         method  => "move",
-        degrees => 90,
         speed   => 100,
     }));
 
@@ -170,14 +101,6 @@ MOVE: {
     ok(defined $resp->{'position'}, 'move, when successful, returns the starting position of each motor');
 
     $ev3mock->reset_mock('get_tacho_position'); # reset get_tacho_position back to default
-
-    $resp = decode_json($server->process_http_request({
-            method  => "move",
-            degrees => -1,
-            speed   => 100,
-    }));
-
-    ok($resp->{'success'}, 'move is successful when unlimited value for degrees is passed');
 }
 
 STOP: {
@@ -197,14 +120,6 @@ TURN: {
 
     $resp = decode_json($server->process_http_request({
         method  => "turn",
-        degrees => -2,
-    }));
-
-    ok(!$resp->{'success'}, 'turn fails with invalid degrees');
-
-    $resp = decode_json($server->process_http_request({
-        method  => "turn",
-        degrees => 90,
         speed   => 200,
     }));
 
@@ -221,7 +136,7 @@ TURN: {
         speed   => 100,
     }));
 
-    ok($resp->{'success'}, 'turn is successful when valid amount of degrees is passed');
+    ok($resp->{'success'}, 'turn is successful when valid speed is passed');
     ok(defined $resp->{'position'}, 'turn, when successful, returns the starting position of each motor');
 
     $ev3mock->reset_mock('get_tacho_position'); # reset get_tacho_position back to default
